379f922fc746015bab46b851a51596f0e145ed62
sarascript <correasg.sara@gmail.com>
2019-10-06 21:29:20 +0200

    Clase Gato y borrar de BBDD

--- src/BBDD.java
+++ src/BBDD.java
@@ -1,3 +1,4 @@
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -6,25 +7,26 @@ import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class BBDD implements AccesoDatos {
-	
+
 	public void escribir(Map<String, String> lista) throws SQLException {
 		ConexionBBDD miConexion = new ConexionBBDD();
 		miConexion.conectar();
 		int r = 0;
 		String cadSQL = null;
-		cadSQL = "INSERT INTO gatos VALUES (" + lista.get("ID") + ", '" + lista.get("Nombre") + "', '" + lista.get("Raza") + "', '" + lista.get("Color") + "')";
+		cadSQL = "INSERT INTO gatos VALUES (" + lista.get("ID") + ", '" + lista.get("Nombre") + "', '"
+				+ lista.get("Raza") + "', '" + lista.get("Color") + "')";
 		r = miConexion.stmt.executeUpdate(cadSQL);
 		System.out.println(r + " registro agregado.");
 	}
-	
+
 	public void escribirTodos(Map<String, String> lista) throws SQLException {
 		for (int i = 0; i < lista.size(); i++) {
-			Iterator<Entry<String,String>> it = lista.entrySet().iterator();
+			Iterator<Entry<String, String>> it = lista.entrySet().iterator();
 			while (it.hasNext()) {
-			  Entry<String,String> e = it.next();
-			  Map<String, String> utilMap = new HashMap();
-			  utilMap.put(e.getKey(), e.getValue());
-			  escribir(utilMap);
+				Entry<String, String> e = it.next();
+				Map<String, String> utilMap = new HashMap();
+				utilMap.put(e.getKey(), e.getValue());
+				escribir(utilMap);
 			}
 		}
 	}
@@ -41,16 +43,47 @@ public class BBDD implements AccesoDatos {
 				auxiliar.put("Nombre", connection.rset.getString(2));
 				auxiliar.put("Raza", connection.rset.getString(3));
 				auxiliar.put("Color", connection.rset.getString(4));
-				auxiliar.entrySet().forEach(entry->{
-				    System.out.println(entry.getValue());  
-				 });
+				auxiliar.entrySet().forEach(entry -> {
+					System.out.println(entry.getValue());
+				});
 				System.out.println("######");
 				System.out.println();
 			}
 		} catch (SQLException s) {
 			s.printStackTrace();
 		}
-		
+		System.out.println(auxiliar.size());
 		return auxiliar;
 	}
+
+	public void vaciar() {
+		try {
+			ConexionBBDD miConexion = new ConexionBBDD();
+			miConexion.conectar();
+			int r = 0;
+			String cadSQL = null;
+			cadSQL = "DELETE * FROM gatos";
+
+			r = miConexion.stmt.executeUpdate(cadSQL);
+			System.out.println(r + " registro borrado.");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public void borrar(int ID) {
+		try {
+			ConexionBBDD miConexion = new ConexionBBDD();
+			miConexion.conectar();
+			int r = 0;
+			String cadSQL = null;
+			cadSQL = "DELETE FROM gatos WHERE gatos.ID = ?";
+			r = miConexion.stmt.executeUpdate(cadSQL);
+			System.out.println(r + " registro borrado.");
+
+		} catch (SQLException s) {
+			System.err.println("Error en la consulta SQL.");
+		}
+	}
+
 }
